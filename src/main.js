37f956bbe6966c78b9eb27ffe31f4eec40cfda87
zwx <853845818@qq.com>
2020-04-11 22:27:59 +0800

    introduce vue-mindmap: import vue-mindmap in main.js; update package.json; insert vue-mindmap component in index.vue.

--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import App from './App.vue'
 import router from './router'
 import ViewUI from 'view-design'
 import 'view-design/dist/styles/iview.css'
+import VueMindmap from 'vue-mindmap'
+import 'vue-mindmap/dist/vue-mindmap.css'
 
 Vue.config.productionTip = false
 Vue.use(ViewUI)
-
+Vue.use(VueMindmap)
 new Vue({
   router,
   render: h => h(App)
