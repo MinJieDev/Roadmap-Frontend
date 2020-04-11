b56dfa7c22e4664f6153ccb42a8e28cdaf93eb8f
Mist <hdl730@163.com>
2020-04-11 21:01:03 +0800

    init

---
+++ src/views/Index.vue
@@ -0,0 +1,137 @@
+<template>
+    <div class="layout">
+        <Layout>
+            <Header>
+                <Menu mode="horizontal" theme="dark" active-name="1">
+                    <div class="layout-logo"></div>
+                    <div class="layout-nav">
+                        <MenuItem name="1">
+                            <Icon type="ios-navigate"></Icon>
+                            Item 1
+                        </MenuItem>
+                        <MenuItem name="2">
+                            <Icon type="ios-keypad"></Icon>
+                            Item 2
+                        </MenuItem>
+                        <MenuItem name="3">
+                            <Icon type="ios-analytics"></Icon>
+                            Item 3
+                        </MenuItem>
+                        <MenuItem name="4">
+                            <Icon type="ios-paper"></Icon>
+                            Item 4
+                        </MenuItem>
+                    </div>
+                </Menu>
+            </Header>
+            <Layout :style="{padding: '0 50px'}">
+                <Breadcrumb :style="{margin: '16px 0px'}">
+                    <BreadcrumbItem>Home</BreadcrumbItem>
+                    <BreadcrumbItem>Components</BreadcrumbItem>
+                    <BreadcrumbItem>Layout</BreadcrumbItem>
+                </Breadcrumb>
+                <Content :style="{padding: '24px 0', minHeight: '500px', background: '#fff'}">
+                    <Layout>
+                        <Sider hide-trigger :style="{background: '#fff'}">
+                            <Menu active-name="1-2" theme="light" width="auto" :open-names="['1']">
+                                <Submenu name="1">
+                                    <template slot="title">
+                                        <Icon type="ios-navigate"></Icon>
+                                        文献栏
+                                    </template>
+                                    <MenuItem name="1-1">Option 1</MenuItem>
+                                    <MenuItem name="1-2">Option 2</MenuItem>
+                                    <MenuItem name="1-3">Option 3</MenuItem>
+                                </Submenu>
+                                <Submenu name="2">
+                                    <template slot="title">
+                                        <Icon type="ios-keypad"></Icon>
+                                        随笔栏
+                                    </template>
+                                    <MenuItem name="2-1">Option 1</MenuItem>
+                                    <MenuItem name="2-2">Option 2</MenuItem>
+                                </Submenu>
+                                <Submenu name="3">
+                                    <template slot="title">
+                                        <Icon type="ios-analytics"></Icon>
+                                        其它
+                                    </template>
+                                    <MenuItem name="3-1">Option 1</MenuItem>
+                                    <MenuItem name="3-2">Option 2</MenuItem>
+                                </Submenu>
+                            </Menu>
+                        </Sider>
+                        <Content :style="{padding: '24px', minHeight: '280px', background: '#fff'}">
+                            Canvas
+                        </Content>
+                        <Sider hide-trigger :style="{background: '#fff'}">
+                            <Menu active-name="1-2" theme="dark" width="auto" :open-names="['1']">
+                                <Submenu name="1">
+                                    <template slot="title">
+                                        <Icon type="ios-navigate"></Icon>
+                                        工具栏
+                                    </template>
+                                    <MenuItem name="1-1">Option 1</MenuItem>
+                                    <MenuItem name="1-2">Option 2</MenuItem>
+                                    <MenuItem name="1-3">Option 3</MenuItem>
+
+                                    <MenuItem name="2-1">Option 1</MenuItem>
+                                    <MenuItem name="2-2">Option 2</MenuItem>
+
+                                    <MenuItem name="3-1">Option 1</MenuItem>
+                                    <MenuItem name="3-2">Option 2</MenuItem>
+                                </Submenu>
+                            </Menu>
+                        </Sider>
+                    </Layout>
+                </Content>
+            </Layout>
+            <Footer class="layout-footer-center"> &copy; footer</Footer>
+        </Layout>
+    </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+  computed: {
+
+  },
+  methods: {
+
+  },
+  created () {
+
+  }
+}
+</script>
+
+<style scoped>
+  .layout{
+    border: 1px solid #d7dde4;
+    background: #f5f7f9;
+    position: relative;
+    border-radius: 4px;
+    overflow: hidden;
+  }
+  .layout-logo{
+    width: 100px;
+    height: 30px;
+    background: #5b6270;
+    border-radius: 3px;
+    float: left;
+    position: relative;
+    top: 15px;
+    left: 20px;
+  }
+  .layout-nav{
+    width: 420px;
+    margin: 0 auto;
+    margin-right: 20px;
+  }
+  .layout-footer-center{
+    text-align: center;
+  }
+</style>
