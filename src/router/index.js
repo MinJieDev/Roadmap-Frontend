2c0dfd581583f822c195bba79c58ade61eed6ca1
zwx <853845818@qq.com>
2020-04-13 21:15:35 +0800

    zwx:introduce vue-mindmap: import vue-mindmap in main.js; update package.json; insert vue-mindmap component in index.vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,43 @@
+// import Vue from 'vue';
+// import Router from 'vue-router';
+// import HelloWorld from '@/components/HelloWorld';
+//
+// Vue.use(Router);
+// const routes = [
+//   {
+//     path: '/',
+//     name: 'Home',
+//     component: () => import(/* webpackChunkName: "about" */ '../views/Index.vue')
+//   }
+// ]
+//
+// export default new Router({
+//   routes: [
+//     {
+//       path: '/',
+//       name: 'HelloWorld',
+//       component: HelloWorld,
+//     },
+//   ],
+//
+// });
 import Vue from 'vue';
-import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
+import VueRouter from 'vue-router';
 
-Vue.use(Router);
+Vue.use(VueRouter);
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld,
-    },
-  ],
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Layout.vue'),
+  },
+];
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes,
 });
+
+export default router;
