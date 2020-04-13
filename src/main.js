738da62ccdba9c2b8d7a53fead78a8b3fde94a67
zwx <853845818@qq.com>
2020-04-13 21:33:41 +0800

    zwx: solve problem: eslint deny </MenuItem>

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
 import ViewUI from 'view-design';
 import 'view-design/dist/styles/iview.css';
 import VueMindmap from 'vue-mindmap';
 import 'vue-mindmap/dist/vue-mindmap.css';
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+
 
 Vue.config.productionTip = false;
 Vue.use(ViewUI);
