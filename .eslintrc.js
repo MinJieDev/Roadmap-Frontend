7768607179a9e8e88b6c9a78e99f6d2128f93d39
zwx <853845818@qq.com>
2020-04-13 21:33:41 +0800

    zwx:introduce vue-mindmap: import vue-mindmap in main.js; update package.json; insert vue-mindmap component in index.vue

--- .eslintrc.js
+++ .eslintrc.js
@@ -45,6 +45,7 @@ module.exports = {
       optionalDependencies: ['test/unit/index.js']
     }],
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    "vue/no-parsing-error": [2, { "x-invalid-end-tag": false }]
   }
 }
