d605e18b38292082a53611803713685821c2ba8c
zwx <853845818@qq.com>
2020-04-14 23:04:15 +0800

    zwx: add vuex/index.js to introduce Vuex; inport vuex in main.js; add examples in views/Layout.vue; rm useless comments in router/index.js;

---
+++ src/vuex/index.js
@@ -0,0 +1,41 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+/* 设置state参数 */
+const state = {
+  example: 'vuex is backing up',
+  txtArr: ['hdlnb', 'hdlswlp'],
+};
+
+const getters = {
+  // eslint-disable-next-line no-shadow
+  getFirstTxtArr(state) {
+    if (state.txtArr.length !== 0) { return state.txtArr[0]; }
+    return '';
+  },
+};
+
+const mutations = {
+  // eslint-disable-next-line no-shadow
+  pushTxtArr(state, str) {
+    state.txtArr.push(str);
+  },
+  popTxtArr() {
+    state.txtArr.pop();
+  },
+};
+
+const actions = {
+
+};
+
+const store = new Vuex.Store({
+  state,
+  getters,
+  mutations,
+  actions,
+});
+
+export default store;
