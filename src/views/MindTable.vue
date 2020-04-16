13a2488c1a37574fafe3fb0d2030750009251a5e
yzy11235 <yzy11235@gmail.com>
2020-04-16 10:17:11 +0800

    add MindTable blow drawing board

---
+++ src/views/MindTable.vue
@@ -0,0 +1,57 @@
+<template>
+  <Table row-key="id"
+         :columns="columns"
+         :data="data"
+         border></Table>
+</template>
+<script>
+export default {
+  name: 'MindTable',
+  data() {
+    return {
+      columns: [
+        {
+          title: 'Title',
+          key: 'title',
+          tree: true,
+        },
+        {
+          title: 'Url',
+          key: 'url',
+        },
+        {
+          title: 'Note',
+          key: 'note',
+        },
+      ],
+      data: [
+        {
+          id: '100',
+          title: 'python',
+          url: 'http://www.wikiwand.com/en/Python_(programming_language)',
+          note: 'Hello Python',
+          children: [
+            {
+              id: '10000',
+              title: 'source',
+              url: 'https://github.com/python/cpython',
+              note: 'original python implementation in c, compiles python code into byte code and interprets the byte code in a evaluation loop',
+            },
+          ],
+        },
+        {
+          id: '101',
+          title: 'help',
+          url: 'https://github.com/vinta/awesome-python',
+          note: 'awesome python',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+
+<style scoped>
+
+</style>
