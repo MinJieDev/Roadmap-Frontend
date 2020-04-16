3beff42cd21c8b24e2595f38772149dbe83c1dac
Mist <hdl730@163.com>
2020-04-17 03:29:53 +0800

    fix lint errors and improve style slightly

--- src/components/Request.vue
+++ src/components/Request.vue
@@ -10,44 +10,21 @@
 </template>
 
 <script>
-import axios from 'axios'
 
-export default {}
-function req (url, method, params = {}, data = {}) {
-  method = method.toUpperCase()
-  let options
-  if (method === 'POST' || method === 'PUT' || method === 'PATCH') {
-    options = {
-      method: method,
-      url: url,
-      params: params,
-      data: data
-    }
-  } else {
-    options = {
-      method: method,
-      url: url,
-      params: params
-    }
-  }
-  try {
-    return axios(options)
-  } catch (e) {
-    console.error(e)
-  }
-}
 
 // 调试函数
 // 注意1：addEventListener可能在部件加载完前运行，需要使用window.onload滞后。
 // 注意2：axios操作的返回是异步操作，对其返回值的处理必须要配合then使用。
-function debugReq () {
-  const url = document.getElementById('url').value.toString()
-  const method = document.getElementById('method').value.toString().toUpperCase()
-  console.info(url)
-  console.info(method)
-  console.info(req(url, method, { _limit: 5 }).then(res => console.info(res.data)))
+import { req } from '../apis/util';
+
+function debugReq() {
+  const url = document.getElementById('url').value.toString();
+  const method = document.getElementById('method').value.toString().toUpperCase();
+  console.info(url);
+  console.info(method);
+  console.info(req(url, method, { _limit: 5 }).then(res => console.info(res.data)));
 }
 window.onload = function () {
-  document.getElementById('try_button').addEventListener('click', debugReq)
-}
+  document.getElementById('try_button').addEventListener('click', debugReq);
+};
 </script>
