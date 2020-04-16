3767e6f3ea38c3c00bd24af45abaab4cb7ff9122
Mist <hdl730@163.com>
2020-04-17 04:04:47 +0800

    add slot into layout and arrange basic router

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <div id="app">
+    <RoadmapLayout>
     <router-view/>
+    </RoadmapLayout>
   </div>
 </template>
 
 <script>
+import RoadmapLayout from './views/RoadmapLayout';
+
 export default {
   name: 'App',
+  components: { RoadmapLayout },
 };
 </script>
 
--- src/views/RoadmapEditorView.vue
+++ src/views/RoadmapEditorView.vue
@@ -1,120 +1,74 @@
 <template>
-  <div class="layout">
-    <Layout>
-      <Header>
-        <Menu mode="horizontal" theme="dark" active-name="1">
-          <div class="layout-logo"></div>
-          <div class="layout-nav">
-            <MenuItem name="1">
-              <Icon type="ios-navigate"></Icon>
-              Item 1
-            </MenuItem>
-            <MenuItem name="2">
-              <Icon type="ios-keypad"></Icon>
-              Item 2
-            </MenuItem>
-            <MenuItem name="3">
-              <Icon type="ios-analytics"></Icon>
-              Item 3
-            </MenuItem>
-            <MenuItem name="4">
-              <Icon type="ios-paper"></Icon>
-              Item 4
-            </MenuItem>
-          </div>
-        </Menu>
-      </Header>
-      <Layout :style="{padding: '0 50px'}">
-        <Breadcrumb :style="{margin: '16px 0px'}">
-          <BreadcrumbItem>Home</BreadcrumbItem>
-          <BreadcrumbItem>Components</BreadcrumbItem>
-          <BreadcrumbItem>Layout</BreadcrumbItem>
-        </Breadcrumb>
-        <Content :style="{padding: '24px 0', minHeight: '500px', background: '#fff'}">
-          <Layout>
-            <Sider hide-trigger :style="{background: '#fff'}">
-              <Menu active-name="1-2" theme="light" width="auto" :open-names="['1']">
-                <Submenu name="1">
-                  <template slot="title">
-                    <Icon type="ios-navigate"></Icon>
-                    文献栏
-                  </template>
-                  <MenuItem name="1-1">Option 1</MenuItem>
-                  <MenuItem name="1-2">Option 2</MenuItem>
-                  <MenuItem name="1-3">Option 3</MenuItem>
-                </Submenu>
-                <Submenu name="2">
-                  <template slot="title">
-                    <Icon type="ios-keypad"></Icon>
-                    随笔栏
-                  </template>
-                  <MenuItem name="2-1">Option 1</MenuItem>
-                  <MenuItem name="2-2">Option 2</MenuItem>
-                </Submenu>
-                <Submenu name="3">
-                  <template slot="title">
-                    <Icon type="ios-analytics"></Icon>
-                    其它
-                  </template>
-                  <MenuItem name="3-1">Option 1</MenuItem>
-                  <MenuItem name="3-2">Option 2</MenuItem>
-                </Submenu>
-              </Menu>
-            </Sider>
-            <Content :style="{padding: '24px', minHeight: '280px', background: '#fff'}">
-              <div :style="{height: '500px', width: '500'}">
-                <p>{{$store.state.example}}</p>
-                <p>{{$store.getters.getFirstTxtArr}}</p>
-<!--  add content here  -->
-                <mindmap
-                  :nodes="nodes"
-                  :connections="connections"
-                  :editable="true"
-                />
-              </div>
-            </Content>
-            <Sider hide-trigger :style="{background: '#fff'}">
-              <Menu active-name="1-2" theme="dark" width="auto" :open-names="['1']">
-                <Submenu name="1">
-                  <template slot="title">
-                    <Icon type="ios-navigate"></Icon>
-                    工具栏
-                  </template>
-                  <MenuItem name="1-1">Option 1</MenuItem>
-                  <MenuItem name="1-2">Option 2</MenuItem>
-                  <MenuItem name="1-3">Option 3</MenuItem>
+  <Layout>
+    <Sider hide-trigger :style="{background: '#fff'}">
+      <Menu active-name="1-2" theme="light" width="auto" :open-names="['1']">
+        <Submenu name="1">
+          <template slot="title">
+            <Icon type="ios-navigate"></Icon>
+            文献栏
+          </template>
+          <MenuItem name="1-1">Option 1</MenuItem>
+          <MenuItem name="1-2">Option 2</MenuItem>
+          <MenuItem name="1-3">Option 3</MenuItem>
+        </Submenu>
+        <Submenu name="2">
+          <template slot="title">
+            <Icon type="ios-keypad"></Icon>
+            随笔栏
+          </template>
+          <MenuItem name="2-1">Option 1</MenuItem>
+          <MenuItem name="2-2">Option 2</MenuItem>
+        </Submenu>
+        <Submenu name="3">
+          <template slot="title">
+            <Icon type="ios-analytics"></Icon>
+            其它
+          </template>
+          <MenuItem name="3-1">Option 1</MenuItem>
+          <MenuItem name="3-2">Option 2</MenuItem>
+        </Submenu>
+      </Menu>
+    </Sider>
+    <Content :style="{padding: '24px', minHeight: '280px', background: '#fff'}">
+      <div :style="{height: '500px', width: '500'}">
+        <!--        <p>{{$store.state.example}}</p>-->
+        <!--        <p>{{$store.getters.getFirstTxtArr}}</p>-->
+        <!--  add content here  -->
+        <mindmap
+          :nodes="nodes"
+          :connections="connections"
+          :editable="true"
+        />
+      </div>
+    </Content>
+    <Sider hide-trigger :style="{background: '#fff'}">
+      <Menu active-name="1-2" theme="dark" width="auto" :open-names="['1']">
+        <Submenu name="1">
+          <template slot="title">
+            <Icon type="ios-navigate"></Icon>
+            工具栏
+          </template>
+          <MenuItem name="1-1">Option 1</MenuItem>
+          <MenuItem name="1-2">Option 2</MenuItem>
+          <MenuItem name="1-3">Option 3</MenuItem>
 
-                  <MenuItem name="2-1">Option 1</MenuItem>
-                  <MenuItem name="2-2">Option 2</MenuItem>
+          <MenuItem name="2-1">Option 1</MenuItem>
+          <MenuItem name="2-2">Option 2</MenuItem>
 
-                  <MenuItem name="3-1">Option 1</MenuItem>
-                  <MenuItem name="3-2">Option 2</MenuItem>
-                </Submenu>
-              </Menu>
-            </Sider>
-          </Layout>
-        </Content>
-        <!-- add below the drawing board -->
-        <div>
-          <MindTable
-            :columns="columns"
-            :data="data"
-          />
-        </div>
-      </Layout>
-      <Footer class="layout-footer-center"> &copy; footer</Footer>
-    </Layout>
-  </div>
+          <MenuItem name="3-1">Option 1</MenuItem>
+          <MenuItem name="3-2">Option 2</MenuItem>
+        </Submenu>
+      </Menu>
+    </Sider>
+  </Layout>
 </template>
+
 <script>
-import MindTable from './MindTable';
 
 export default {
-  name: 'MyMindmap',
-  components: { MindTable },
+  name: 'RoadmapEditor',
   data() {
     return {
-      title: 'learn anything - programming - programming languages - python',
       nodes: [{
         text: 'python',
         url: 'http://www.wikiwand.com/en/Python_(programming_language)',
@@ -308,92 +262,61 @@ export default {
         nodes: [],
       },
       ],
-      connections: [{
-        source: 'python',
-        target: 'basics',
-        curve: {
-          x: -43.5535,
-          y: 299.545,
-        },
-      },
-      {
-        source: 'help',
-        target: 'python',
-        curve: {
-          x: -78.1206,
-          y: -114.714,
-        },
-      },
-      {
-        source: 'basics',
-        target: 'python libraries  ️',
-        curve: {
-          x: 29.6649,
-          y: 80.1111,
-        },
-      },
-      {
-        source: 'basics',
-        target: 'package manager',
-        curve: {
-          x: -103.841,
-          y: 49.5548,
-        },
-      },
-      {
-        source: 'package manager',
-        target: 'pip',
-        curve: {
-          x: -19.7824,
-          y: 57.2671,
-        },
-      },
-      {
-        source: 'articles',
-        target: 'help',
-        curve: {
-          x: -238.287,
-          y: -54.4818,
-        },
-      },
-      ],
+      connections:
+          [{
+            source: 'python',
+            target: 'basics',
+            curve: {
+              x: -43.5535,
+              y: 299.545,
+            },
+          },
+          {
+            source: 'help',
+            target: 'python',
+            curve: {
+              x: -78.1206,
+              y: -114.714,
+            },
+          },
+          {
+            source: 'basics',
+            target: 'python libraries  ️',
+            curve: {
+              x: 29.6649,
+              y: 80.1111,
+            },
+          },
+          {
+            source: 'basics',
+            target: 'package manager',
+            curve: {
+              x: -103.841,
+              y: 49.5548,
+            },
+          },
+          {
+            source: 'package manager',
+            target: 'pip',
+            curve: {
+              x: -19.7824,
+              y: 57.2671,
+            },
+          },
+          {
+            source: 'articles',
+            target: 'help',
+            curve: {
+              x: -238.287,
+              y: -54.4818,
+            },
+          },
+          ],
     };
-  },
-  computed: {},
-  methods: {},
-  created() {
-
   },
 };
 </script>
 
 <style scoped>
-  .layout {
-    border: 1px solid #d7dde4;
-    background: #f5f7f9;
-    position: relative;
-    border-radius: 4px;
-    overflow: hidden;
-  }
 
-  .layout-logo {
-    width: 100px;
-    height: 30px;
-    background: #5b6270;
-    border-radius: 3px;
-    float: left;
-    position: relative;
-    top: 15px;
-    left: 20px;
-  }
-
-  .layout-nav {
-    width: 420px;
-    margin: 0 auto;
-    margin-right: 20px;
-  }
-
-  .layout-footer-center {
-    text-align: center;
-  }
 </style>
