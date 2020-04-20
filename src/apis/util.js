148ea88cb18996539be7a64225cf30f3bf8bbaa2
sinoyou <37182679+sinoyou@users.noreply.github.com>
2020-04-21 00:49:14 +0800

    Add axios interceptor for error status handle in response. (#32)
    * Add axios interceptor for error status handle in response.
    
    * rename as code, add raw error.
    
    * add comment

--- src/apis/util.js
+++ src/apis/util.js
@@ -1,5 +1,37 @@
 import axios from 'axios';
 
+// axios with interceptor
+// eslint-disable-next-line no-unused-vars
+const instance = axios.create({
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+  },
+});
+
+instance.interceptors.response.use(
+  response => response,
+  (error) => {
+    if (error.response) {
+      // code: 错误状态码， response：错误响应信息，error：原始错误信息
+      switch (error.response.status) {
+        case 400:
+          return Promise.reject({ code: 400, response: error.response, error }); // 客户端请求有语法错误
+        case 401:
+          return Promise.reject({ code: 401, response: error.response, error }); // 请求未经授权
+        case 404:
+          return Promise.reject({ code: 404, response: error.response, error }); // 页面未找到
+        case 403:
+          return Promise.reject({ code: 403, response: error.response, error }); // Bad Gateway
+        case 500:
+          return Promise.reject({ code: 500, response: error.response, error }); // Server Error
+        default:
+          return Promise.reject({ code: -1, response: error.response, error }); // 不常见错误
+      }
+    } else {
+      return Promise.reject({ code: -1, response: {}, error });
+    }
+  },
+);
 
 // eslint-disable-next-line import/prefer-default-export
 export const req = (url, _method, params = {}, data = {}) => {
@@ -19,9 +51,5 @@ export const req = (url, _method, params = {}, data = {}) => {
       params,
     };
   }
-  try {
-    return axios(options);
-  } catch (e) {
-    throw e;
-  }
+  return instance(options);
 };
