2038d81ea7fba0daece2439d602c255711beffc3
zwx <853845818@qq.com>
2020-04-20 21:22:10 +0800

    zwx: back-end api test: enable GET article-list and render

--- config/index.js
+++ config/index.js
@@ -10,7 +10,13 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {'/api': {
+        target: 'http://127.0.0.1:8000/',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': '/api'
+        }
+      }},
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
