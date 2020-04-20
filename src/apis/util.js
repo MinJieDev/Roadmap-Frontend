c4597764d75bd58cda1d869c1bff25a2cab0a7b1
sinoyou <youzn99@qq.com>
2020-04-21 00:04:29 +0800

    Add axios interceptor for error status handle in response.

--- src/apis/util.js
+++ src/apis/util.js
@@ -1,5 +1,36 @@
 import axios from 'axios';
 
+// axios with interceptor
+// eslint-disable-next-line no-unused-vars
+const instance = axios.create({
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+  },
+});
+
+instance.interceptors.response.use(
+  response => response,
+  (error) => {
+    if (error.response) {
+      switch (error.response.status) {
+        case 400:
+          return Promise.reject({ error: 400, response: error.response }); // 客户端请求有语法错误
+        case 401:
+          return Promise.reject({ error: 401, response: error.response }); // 请求未经授权
+        case 404:
+          return Promise.reject({ error: 404, response: error.response }); // 页面未找到
+        case 403:
+          return Promise.reject({ error: 403, response: error.response }); // Bad Gateway
+        case 500:
+          return Promise.reject({ error: 500, response: error.response }); // Server Error
+        default:
+          return Promise.reject({ error: -1, response: error.response }); // 不常见错误
+      }
+    } else {
+      return error;
+    }
+  },
+);
 
 // eslint-disable-next-line import/prefer-default-export
 export const req = (url, _method, params = {}, data = {}) => {
@@ -19,9 +50,5 @@ export const req = (url, _method, params = {}, data = {}) => {
       params,
     };
   }
-  try {
-    return axios(options);
-  } catch (e) {
-    throw e;
-  }
+  return instance(options);
 };
--- src/components/Request.vue
+++ src/components/Request.vue
@@ -17,13 +17,22 @@ git<template>
 // 注意2：axios操作的返回是异步操作，对其返回值的处理必须要配合then使用。
 import { req } from '../apis/util';
 
+export default {};
+
 function debugReq() {
   const url = document.getElementById('url').value.toString();
   const method = document.getElementById('method').value.toString().toUpperCase();
+  // eslint-disable-next-line no-console
   console.info(url);
+  // eslint-disable-next-line no-console
   console.info(method);
-  console.info(req(url, method, { _limit: 5 }).then(res => console.info(res.data)));
+  // eslint-disable-next-line no-console
+  req(url, method, { _limit: 5 })
+    // eslint-disable-next-line no-console
+    .then(response => console.info(response.data)).catch(error => console.info(error.error));
 }
+
+// eslint-disable-next-line func-names
 window.onload = function () {
   document.getElementById('try_button').addEventListener('click', debugReq);
 };
