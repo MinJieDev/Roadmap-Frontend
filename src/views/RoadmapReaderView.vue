e17f754c114610eaee82d7cfe0c4fca01447c8c2
Mist <hdl730@163.com>
2020-04-23 21:09:31 +0800

    connect reference automatically in reader

--- src/views/RoadmapReaderView.vue
+++ src/views/RoadmapReaderView.vue
@@ -4,9 +4,9 @@
       <div  id="roadmap-title">
         {{roadMapTitle}}
       </div>
-      <mindmap
+      <roadmap
         :nodes="nodes"
-        :connections="connections"
+        :connections="mergedConnections"
         :editable="true"
         :key="repaint"
       />
@@ -22,12 +22,15 @@ import _ from 'lodash';
 import Vue from 'vue';
 import errPush from '../components/ErrPush';
 import { getRoadmap } from '../apis/RoadmapEditorApis';
+import { req } from '../apis/util';
+import Roadmap from '../components/roadmap/Roadmap';
 
 Vue.prototype._ = _;
 
 export default {
-  name: 'RoadmapEditor',
+  name: 'RoadmapReader',
   components: {
+    Roadmap,
   },
   data() {
     return {
@@ -38,21 +41,56 @@ export default {
       ],
       connections: [
       ],
+      articles: [],
     };
   },
-  computed: {
-  },
-  mounted() {
+  async mounted() {
     this.roadMapId = this.$route.query.selected;
-    getRoadmap(this.roadMapId).then((res) => {
-      this.nodes = JSON.parse(res.data.text).nodes;
-      this.connections = JSON.parse(res.data.text).connections;
-      this.roadMapTitle = res.data.title;
+    try {
+      this.articles = (await req('/api/articles/', 'GET')).data;
+      const roadmapData = (await getRoadmap(this.roadMapId)).data;
+      this.nodes = JSON.parse(roadmapData.text).nodes;
+      this.connections = JSON.parse(roadmapData.text).connections;
+      this.roadMapTitle = roadmapData.title;
       this.repaintMindMap();
       this.$Notice.success({ title: `Roadmap loaded, id: ${this.roadMapId}` });
-    }).catch(() => {
+    } catch (e) {
       errPush(this, '4000', true);
-    });
+    }
+  },
+  computed: {
+    /**
+     * auto generated reference connections
+     * @type {Array}
+     */
+    refConnections() {
+      let conn = [];
+      let articleNodes = _.filter(this.nodes, node => (node.category === 'article'));
+      articleNodes = _.map(articleNodes, (node) => {
+        const article = _.find(this.articles, atc => (atc.title === node.text));
+        return { ...node, article };
+      });
+      _.forEach(articleNodes, (ni) => {
+        _.forEach(articleNodes, (nj) => {
+          if (_.includes(ni.article.article_references, nj.article.id)) {
+            conn = _.concat(conn, {
+              source: nj.text,
+              target: ni.text,
+              type: 'ref',
+            });
+          }
+        });
+      });
+      return conn;
+    },
+
+    /**
+     * simple connections and reference connections
+     * @returns {*[]}
+     */
+    mergedConnections() {
+      return [...this.connections, ...this.refConnections];
+    },
   },
   methods: {
     handleClkEdit() {
