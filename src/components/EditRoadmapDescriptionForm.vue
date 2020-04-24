a95cfb9532b9696910e44e509f7cd235b0ee6411
zwx <853845818@qq.com>
2020-04-25 02:13:43 +0800

    zwx: editor: roadmap description UR

---
+++ src/components/EditRoadmapDescriptionForm.vue
@@ -0,0 +1,35 @@
+<template>
+    <Modal
+      v-model="editDescriptionForm"
+      title="Edit Description"
+      @on-ok="ok"
+      @on-cancel="cancel"
+      @on-trig="handleTrig">
+      <p>Description</p>
+      <Input v-model="newDescription" type="textarea" placeholder="Enter Description..."/>
+    </Modal>
+</template>
+<script>
+export default {
+  name: 'editDescriptionForm',
+  props: ['description'],
+  data() {
+    return {
+      editDescriptionForm: false,
+      newDescription: '',
+    };
+  },
+  methods: {
+    ok() {
+      this.$emit('description-edited', this.newDescription);
+    },
+    cancel() {
+      //  this.$Message.info('Clicked cancel');
+    },
+    handleTrig() {
+      this.newDescription = this.description;
+      this.editDescriptionForm = true;
+    },
+  },
+};
+</script>
