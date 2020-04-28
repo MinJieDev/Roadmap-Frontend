b8a6dd42dfebe091b7c7814abba34e9bfb3ea9e7
zwx <853845818@qq.com>
2020-04-29 02:25:24 +0800

    zwx: upload description when click save, donot upload if -1; change arrow parameter for better view; comment some unused buttons in editor view

--- src/apis/RoadmapEditorApis.js
+++ src/apis/RoadmapEditorApis.js
@@ -1,11 +1,11 @@
 import { req } from '../apis/util';
 
 
-export const createRoadmap = (roadmapTitle, nodes, connections) => req('/api/road_maps/', 'POST', {},
-  { text: JSON.stringify({ nodes, connections }), title: roadmapTitle, user: 1 });
+export const createRoadmap = (roadmapTitle, nodes, connections, description) => req('/api/road_maps/', 'POST', {},
+  { text: JSON.stringify({ nodes, connections }), title: roadmapTitle, user: 1, description });
 
-export const updateRoadmap = (id, roadmapTitle, nodes, connections) => req(`/api/road_maps/${id}/`, 'PUT', {},
-  { text: JSON.stringify({ nodes, connections }), title: roadmapTitle });
+export const updateRoadmap = (id, roadmapTitle, nodes, connections, description) => req(`/api/road_maps/${id}/`, 'PUT', {},
+  { text: JSON.stringify({ nodes, connections }), title: roadmapTitle, description });
 
 export const getRoadmap = id => req(`/api/road_maps/${id}/`, 'GET');
 
