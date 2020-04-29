8da172814072f40e79769d4756860768ff66fa9f
sinoyou <37182679+sinoyou@users.noreply.github.com>
2020-04-30 00:25:06 +0800

    Yzn add register view (#75)
    * File initial.
    
    * 1. fix bug of '登录'
    2. 增加注册按钮
    
    * 1. 添加基础的注册界面
    2. 用户名，密码和邮箱的输入检查
    3. 随机数验证码生成检查
    4. 注册成功后的跳转

--- src/router/index.js
+++ src/router/index.js
@@ -19,6 +19,11 @@ const routes = [
     component: () => import('../views/UserLoginView.vue'),
   },
   {
+    path: '/user_register',
+    name: 'Register',
+    component: () => import('../views/UserRegisterView'),
+  },
+  {
     path: '/editor',
     name: 'Editor',
     component: () => import(/* webpackChunkName: "about" */ '../views/RoadmapEditorView.vue'),
---
+++ src/views/UserRegisterView.vue
@@ -0,0 +1,91 @@
+<template>
+  <div>
+    <div id="logo-view">
+      <img class="inner_label login_logo" src="../assets/LoginLogo.png" alt="No Image">
+    </div>
+    <div id="login-view">
+      <i-input v-model="userName" placeholder="注册用户名"
+               style="width: 300px " />
+      <br>
+      <i-input type="password" v-model="password" placeholder="注册密码"
+               style="width: 300px "/>
+      <br>
+      <i-input v-model="email" placeholder="注册邮箱"
+               style="width: 300px "/>
+      <br>
+      <i-input disabled v-model="check_mes" style="width: 200px "/>
+      <i-input v-model="check_code" style="width: 100px "/>
+      <br>
+      <br>
+      <i-button
+        type="primary"
+        @click.native="register">
+        注册
+      </i-button>
+    </div>
+  </div>
+</template>
+
+
+<script>
+
+import { reqNoAuth } from '../apis/util';
+import errPush from '../components/ErrPush';
+import router from '../router';
+
+export default {
+  data() {
+    return {
+      userName: '',
+      password: '',
+      email: '',
+      check_mes: '',
+      check_code: '',
+      add_a: '',
+      add_b: '',
+    };
+  },
+  mounted() {
+    this.add_a = Math.round(Math.random() * 10);
+    this.add_b = Math.round(Math.random() * 10);
+    this.check_mes = `验证码：计算 ${this.add_a} + ${this.add_b} = `;
+  },
+  methods: {
+    register() {
+      if (!this.userName.trim() || !this.password.trim() || !this.email.trim()) {
+        errPush(this, '5010');
+      } else if (this.check_code.trim() !== `${this.add_a + this.add_b}`) {
+        errPush(this, '0000', 'false', '无效验证码');
+      } else {
+        const tempData = {
+          username: this.userName.trim(),
+          password: this.password.trim(),
+          email: this.email.trim(),
+        };
+        reqNoAuth('/api/users/', 'post', tempData)
+          .then(() => {
+            router.push('/user_login');
+          })
+          .catch(res => this.handle_error(res))
+        ;
+      }
+    },
+    handle_error(response) {
+      errPush(this, '0000', false, '网络错误', `${response.code}`);
+    },
+  },
+};
+</script>
+
+<style scoped>
+  #logo-view{
+    text-align:center;
+    font-size: 24px;
+    padding: 12px;
+  }
+  #login-view{
+    text-align:center;
+    font-size: 24px;
+    padding: 12px;
+  }
+</style>
