3465cc265bd412b87bd37e4c610615ad317626fd
Mist <hdl730@163.com>
2020-05-01 16:05:30 +0800

    add favicon and change title

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -67,6 +67,7 @@ const webpackConfig = merge(baseWebpackConfig, {
         ? 'index.html'
         : config.build.index,
       template: 'index.html',
+      favicon: 'src/assets/favicon.ico',
       inject: true,
       minify: {
         removeComments: true,
