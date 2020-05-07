303e0ec7132f041514d76237c82f3be4b4be5f98
sinoyou <youzn99@qq.com>
2020-05-07 19:45:31 +0800

    add link.

--- src/views/WelcomeCardView.vue
+++ src/views/WelcomeCardView.vue
@@ -1,27 +1,33 @@
 <template>
 <div>
-  <Row type="flex" justify="center" align="center" class="code-row-bg">
+  <Row type="flex" justify="center" align="middle">
     <i-col span="6">
       <Card style="width:320px; height: 260px">
         <div style="text-align:center">
-          <img src="../assets/welcome/mindmap.png" style="width:70%" alt="Load Fail">
-          <h3>新建路书</h3>
+          <a href="editor">
+            <img src="../assets/welcome/mindmap.png" style="width:70%" alt="Load Fail">
+            <h3>新建路书</h3>
+          </a>
         </div>
       </Card>
     </i-col>
     <i-col span="6">
       <Card style="width:320px; height: 260px">
         <div style="text-align:center">
-          <img src="../assets/welcome/books.png" style="width:70%" alt="Loading Fail">
-          <h3>文献管理</h3>
+          <a href="articleTable">
+            <img src="../assets/welcome/books.png" style="width:70%" alt="Load Fail">
+            <h3>文献管理</h3>
+          </a>
         </div>
       </Card>
     </i-col>
     <i-col span="6">
       <Card style="width:320px; height: 260px">
         <div style="text-align:center">
-          <img src="../assets/welcome/folder.png" style="width:70%" alt="Load Fail">
-          <h3>路书管理</h3>
+          <a href="RoadMapTable">
+            <img src="../assets/welcome/folder.png" style="width:70%" alt="Load Fail">
+            <h3>路书管理</h3>
+          </a>
         </div>
       </Card>
     </i-col>
