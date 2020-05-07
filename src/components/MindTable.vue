3551d9f110f766f737a5e9593c59aeee0a13ba58
yzy11235 <43177119+yzy11235@users.noreply.github.com>
2020-05-07 23:52:30 +0800

    ym table expand added (#107)
    Co-authored-by: yzy11235 <yzy11235@gmail.com>

--- src/components/MindTable.vue
+++ src/components/MindTable.vue
@@ -44,6 +44,7 @@ import _ from 'lodash';
 import bibtexParse from 'bibtex-parse-js';
 import { pushErr } from '../components/ErrPush';
 import ItemEditor from './TableItemEditor';
+import MindTableExpand from './MindTableExpand';
 import { deleteMTdata, createMTdata, changeMTdata } from '../apis/MindTableEditorApis';
 
 export default {
@@ -54,7 +55,7 @@ export default {
       required: true,
     },
   },
-  components: { ItemEditor },
+  components: { ItemEditor, MindTableExpand },
   data() {
     return {
       drawer: false,
@@ -68,25 +69,34 @@ export default {
       },
       columns: [
         {
+          type: 'expand',
+          width: 50,
+          render: (h, params) => h(MindTableExpand, {
+            props: {
+              row: params.row,
+            },
+          }),
+        },
+        {
           title: 'Title',
           key: 'title',
-          width: 230,
+          width: 300,
         },
         {
           title: 'Author',
           key: 'author',
-          width: 150,
-        },
-        {
-          title: 'Url',
-          key: 'url',
           width: 200,
         },
-        {
-          title: 'Journal/Conference',
-          key: 'journal',
-          width: 200,
-        },
+        // {
+        //   title: 'Url',
+        //   key: 'url',
+        //   width: 200,
+        // },
+        // {
+        //   title: 'Journal/Conference',
+        //   key: 'journal',
+        //   width: 200,
+        // },
         {
           title: 'Note',
           key: 'note',
---
+++ src/components/MindTableExpand.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <Row class="expand-row">
+      <Col span="8">
+        <span class="expand-key">Url: </span>
+        <span class="expand-value">{{ row.url }}</span>
+      </Col>
+      <Col span="8">
+        <span class="expand-key">Journal/Conference: </span>
+        <span class="expand-value">{{ row.journal }}</span>
+      </Col>
+      <Col span="8">
+        <span class="expand-key">Year: </span>
+        <span class="expand-value">{{ row.year }}</span>
+      </Col>
+    </Row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MindTableExpand',
+  props: {
+    row: Object,
+  },
+};
+</script>
+
+<style scoped>
+  .expand-row{
+    margin-bottom: 16px;
+  }
+</style>
