d0bbae18daba02289e1fbc7fddb579ccf7280a49
Lebway <luojunyu@buaa.edu.cn>
2020-05-21 23:48:48 +0800

    - ADD full path to stack

--- src/components/Breadcrumb.js
+++ src/components/Breadcrumb.js
@@ -1,27 +1,21 @@
 // eslint-disable-next-line no-array-constructor
 window.routerStack = new Array();
-window.routerStack.push('Welcome');
+window.routerStack.push({ name: 'Welcome', nickName: '主页', fullPath: '\\', level: 1 });
 
-export default function pushRouter(name) {
-  const routerLevelDict = {
-    Welcome: 1,
-    Login: 2,
-    Register: 2,
-    ArticleTable: 2,
-    RoadmapTable: 2,
-    ArticleMarkDownEditor: 10,
-    Editor: 5,
-    Reader: 5,
-    undefined: 50,
-    null: 50,
-    ' ': 50,
-  };
-  // window.console.info('[DEBUG] routerLevelDict[name]: ', routerLevelDict[name]);
+export default function pushRouter(name, nickName, fullPath, level) {
+  let hasHomePage = false;
   for (let i = 0; i < window.routerStack.length; i += 1) {
-    if (routerLevelDict[window.routerStack[i]] >= routerLevelDict[name]) {
+    if (window.routerStack[i].nickName === '主页') {
+      hasHomePage = true;
+    } else if ((window.routerStack[i].level >= level || window.routerStack[i].name === undefined)) {
       window.routerStack.splice(i, 1);
       i -= 1;
     }
   }
-  window.routerStack.push(name);
+  if (!hasHomePage) {
+    window.routerStack.push({ name: 'Welcome', nickName: '主页', fullPath: '\\', level: 1 });
+  }
+  if (level > 1) {
+    window.routerStack.push({ name, nickName, fullPath, level });
+  }
 }
