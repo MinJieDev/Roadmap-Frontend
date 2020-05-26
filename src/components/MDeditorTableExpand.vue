7d29a43b0bb189f87dd9d0afbd21188f1a21a174
yzy11235 <yzy11235@gmail.com>
2020-05-26 19:01:27 +0800

    table colunms finished

---
+++ src/components/MDeditorTableExpand.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+  export default {
+    name: 'MDeditorTableExpand'
+  };
+</script>
+
+<style scoped>
+
+</style>
--- src/components/MarkdownEditorTable.vue
+++ src/components/MarkdownEditorTable.vue
@@ -10,15 +10,138 @@
 </template>
 
 <script>
+import _ from 'lodash';
+import MDeditorTableExpand from './MDeditorTableExpand';
+import ItemEditor from './TableItemEditor';
+import { pushErr } from '../components/ErrPush';
+import { createMDnote, deleleMDnote, changeMDnote } from '../apis/MarkdownEditorApis';
+
 export default {
   name: 'MarkdownEditorTable',
   props: {
-
+    tableData: {
+      type: Array,
+      required: true,
+    },
+    noteTotal: {
+      type: Number,
+      required: true,
+    },
   },
+  components: { ItemEditor, MDeditorTableExpand },
   data() {
     return {
-      columns: [],
+      noteData: {
+        title: '',
+        state: false,
+        abstract: '',
+      },
+      page: {
+        current: 1,
+      },
+      columns: [
+        {
+          type: 'expand',
+          width: 50,
+          render: (h, params) => h(MDeditorTableExpand, {
+            props: {
+              row: params.row,
+            },
+          }),
+        },
+        {
+          type: 'selection',
+          width: 60,
+          align: 'center',
+        },
+        {
+          title: 'Title',
+          key: 'title',
+          // width: 400,
+        },
+        {
+          title: 'State',
+          key: 'status',
+          align: 'center',
+          width: 100,
+          render: (h, params) => {
+            const type = params.row.state === false ? 'error' : 'primary';
+            const text = params.row.state === false ? 'Unfinished' : 'Finished';
+            return h('Button', {
+              props: {
+                type,
+                size: 'small',
+              },
+              on: {
+                click: () => {
+                  this.changeNoteStatus(params.index);
+                },
+              },
+            }, text);
+          },
+        },
+        {
+          title: 'Action',
+          key: 'action',
+          align: 'center',
+          render: (h, params) => h('div', [
+            h('Button', {
+              props: {
+                icon: 'md-document',
+                size: 'small',
+                type: 'text',
+              },
+              style: {
+                marginRight: '5px',
+              },
+              on: {
+                click: () => {
+                  this.onView(params.index);
+                },
+              },
+            },
+            '查看笔记'),
+            h('Button', {
+              props: {
+                type: 'text',
+                size: 'small',
+                icon: 'md-create',
+              },
+              style: {
+                marginRight: '5px',
+              },
+              on: {
+                click: () => {
+                  this.editNote(params.index);
+                },
+              },
+            },
+            '编辑笔记'),
+            h('Button', {
+              props: {
+                icon: 'md-trash',
+                size: 'small',
+                type: 'text',
+              },
+              style: {
+                marginRight: '5px',
+              },
+              on: {
+                click: () => {
+                  this.onDelete(params.index);
+                },
+              },
+            },
+            '删除文献'),
+          ]),
+        },
+      ],
     };
+  },
+  methods: {
+    changeNoteStatus(index) {
+
+    },
   },
 };
 </script>
