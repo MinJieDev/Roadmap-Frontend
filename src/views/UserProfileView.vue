3aafb9543492b1e8ed14829ed620925b6b8ab062
yzy11235 <yzy11235@gmail.com>
2020-05-27 19:45:36 +0800

    sider & content finished

--- src/views/UserProfileView.vue
+++ src/views/UserProfileView.vue
@@ -1,12 +1,16 @@
 <template>
-  <Row>
-    <Col span="8">
+  <Layout>
+    <Sider :style="{background: '#fff'}">
       <Menu theme="light">
-        <MenuItem name="1">
+        <MenuItem
+          name="1"
+          @click.native="openUserProfile">
           <Icon type="md-list-box" />
           档案管理
         </MenuItem>
-        <MenuItem name="2">
+        <MenuItem
+          name="2"
+          @click.native="openUserInterest">
           <Icon type="md-aperture" />
           兴趣管理
         </MenuItem>
@@ -15,18 +19,83 @@
             <Icon type="ios-stats" />
             内容统计
           </template>
-          <MenuItem name="2-1">文献统计</MenuItem>
-          <MenuItem name="2-2">路书统计</MenuItem>
-          <MenuItem name="2-3">随笔统计</MenuItem>
+          <MenuItem
+            name="3-1"
+            @click.native="openArticleStatcs">
+            文献统计
+          </MenuItem>
+          <MenuItem
+            name="3-2"
+            @click.native="openRoadmapStatcs">
+            路书统计
+          </MenuItem>
+          <MenuItem
+            name="3-3"
+            @click.native="openEssayStatcs">
+            随笔统计
+          </MenuItem>
         </Submenu>
       </Menu>
-    </Col>
-  </Row>
+    </Sider>
+    <Content
+      :style="{padding: '60px', minHeight: '280px', background: '#fff'}">
+      <div v-if="this.content==='profile'">
+        userProfile
+      </div>
+      <div v-else-if="this.content==='interest'">
+        userInterest
+      </div>
+      <div v-else-if="content==='artcSt'">
+        article
+      </div>
+      <div v-else-if="content==='roadmapSt'">
+        roadmap
+      </div>
+      <div v-else-if="content==='essaySt'">
+        essay
+      </div>
+    </Content>
+  </Layout>
 </template>
 
 <script>
 export default {
   name: 'UserProfileView',
+  data() {
+    return {
+      content: 'profile',
+      // profile, interest, artcSt, roadmapSt, essaySt
+      userData: {
+
+      },
+      interestData: {
+
+      },
+      articleTotal: 0,
+      articles: [],
+      roadmapTotal: 0,
+      roadmaps: [],
+      essayTotal: 0,
+      essays: [],
+    };
+  },
+  methods: {
+    openUserProfile() {
+      this.content = 'profile';
+    },
+    openUserInterest() {
+      this.content = 'interest';
+    },
+    openArticleStatcs() {
+      this.content = 'artcSt';
+    },
+    openRoadmapStatcs() {
+      this.content = 'roadmapSt';
+    },
+    openEssayStatcs() {
+      this.content = 'essaySt';
+    },
+  },
 };
 </script>
 
